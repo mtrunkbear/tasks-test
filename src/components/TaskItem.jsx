8e9077238f72f723a7e540e5d14cd154e6458064
mtrunkbear <migueltroncosogaldames@gmail.com>
2023-08-25 20:04:22 -0400

    Added filter to TaskList, and improve styled in TaskItem with mui and emotion

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,15 +1,61 @@
-import React from 'react';
+import React from "react";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import CancelIcon from "@mui/icons-material/Cancel";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { Checkbox, Paper, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+import emotionStyled  from "@emotion/styled";
 
 const TaskItem = ({ task }) => {
+  const getTaskState = () => {
+    const today = new Date();
+    const dueDate = new Date(task.dueDate);
+
+    if (task.completed) {
+      return "completed";
+    } else if (dueDate < today) {
+      return "overdue";
+    } else if (dueDate >= today) {
+      return "pending";
+    }
+  };
+
+  const state = getTaskState();
+
   return (
-    <div>
-      <p>{task.description}</p>
-      <p>{task.creationDate}</p>
-      <p>{task.dueDate}</p>
-      <input type="checkbox" checked={task.completed} />
-      {/* Implement other details */}
-    </div>
+    <TaskItemContainer elevation={2}>
+      <Checkbox checked={task.completed} />
+      <TaskDescription>{task.description}</TaskDescription>
+      <DateInput type="date" value={task.creationDate}  />
+      {TaskIcons[state]}
+    </TaskItemContainer>
   );
 };
 
 export default TaskItem;
+
+const DateInput = emotionStyled.input`
+  border: "none",
+  outline: "none",
+  backgroundColor: "transparent",
+  color: "inherit",
+  fontSize: "inherit",
+`;
+
+const TaskItemContainer = styled(Paper)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(2),
+  marginBottom: theme.spacing(2),
+}));
+
+const TaskDescription = styled(Typography)(({ theme }) => ({
+  flex: 1,
+  marginRight: theme.spacing(2),
+}));
+
+const TaskIcons = {
+  completed: <CheckCircleIcon />,
+  overdue: <CancelIcon />,
+  pending: <AccessTimeIcon />,
+};
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,23 +1,51 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchTasks } from '../features/tasks/taskSlice';
+import { fetchTasks, addTask } from '../features/tasks/taskSlice';
 import TaskItem from './TaskItem';
 import AddTaskForm from './AddTaskForm';
 
 const TaskList = () => {
   const dispatch = useDispatch();
   const tasks = useSelector((state) => state.tasks);
+  const [filter, setFilter] = useState('default'); // default, dateCreated, dateDue, state
 
   useEffect(() => {
     dispatch(fetchTasks());
   }, [dispatch]);
 
+  const handleAddTask = (task) => {
+    dispatch(addTask(task));
+  };
+
+  const filteredTasks = [...tasks].sort((a, b) => {
+    if (filter === 'dateCreated') {
+      return new Date(b.creationDate) - new Date(a.creationDate);
+    }
+    if (filter === 'dateDue') {
+      return new Date(a.dueDate) - new Date(b.dueDate);
+    }
+    if (filter === 'state') {
+      return b.completed - a.completed;
+    }
+    return 0;
+  });
+
   return (
     <div>
-      {tasks.map((task) => (
+      <h1>Task List</h1>
+      <label>
+        Filter & Sort:
+        <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+          <option value="default">Default</option>
+          <option value="dateCreated">Date Created (Newest)</option>
+          <option value="dateDue">Due Date (Earliest)</option>
+          <option value="state">State (Priority)</option>
+        </select>
+      </label>
+      <AddTaskForm onAddTask={handleAddTask} />
+      {filteredTasks.map((task) => (
         <TaskItem key={task.id} task={task} />
       ))}
-      <AddTaskForm/>
     </div>
   );
 };
